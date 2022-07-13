e4e5e78577311ccb7e6fdaad0d6ca2393326ca54
frenchfryy <64445218+frenchfryy@users.noreply.github.com>
2022-07-13 01:18:40 -0700

    Update app.js

---
+++ toDoApp/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector('#new-task-input');
+    const list_el = document.querySelector('#list');
+    
+    form.addEventListener('submit', (s) => {
+        s.preventDefault ();
+    })
+})
